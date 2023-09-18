35dcac2cb7ba0d52e1e939a0a415085c0bb740b2
smokedplus <d4nksc0p3@gmail.com>
2023-09-18 18:55:24 -0500

    Mostly finished question logic, termination of game to be added later.

--- game.py
+++ game.py
@@ -14,43 +14,51 @@ class Question():
 set_of_questions = []
 
 def play():
-    question_number = 0
-    for question in set_of_questions:
-        question_number += 1
-        user_answer = input(question.question + "\n"
-        + str(random.sample(question.answers, 4)) + "\n"+
-        "Type the answer below. \n")
-        if user_answer == question.correct_answer:
-            print("Correct!")
-            if question_number == 1:
-                money = "$500"
-            elif question_number == 2:
-                money = "$1000"
-            elif question_number == 3:
-                money = "$2000"
-            elif question_number == 4:
-                money = "$3000"
-            elif question_number == 5:
-                money = "$5000"
-            elif question_number == 6:
-                money = "$7000"
-            elif question_number == 7:
-                money = "$10000"
-            elif question_number == 8:
-                money = "$20000"
-            elif question_number == 9:
-                money = "$30000"
-            elif question_number == 10:
-                money = "$5000"
-            elif question_number == 11:
-                money = "$100000"
-            elif question_number == 12:
-                money = "$250000"
-            elif question_number == 13:
-                money = "$500000"
-            elif question_number == 14:
-                money = "$1000000"
-            print("You have " + money + ".\n")
+    while True:
+        start = input("Who wants to be a millionaire? Print Me! to start.")
+        if start == "Me!":
+            question_number = 0
+            for question in set_of_questions:
+                question_number += 1
+                user_answer = input(question.question + "\n"
+                + str(random.sample(question.answers, 4)) + "\n"+
+                "Type the answer below. \n")
+                if user_answer == question.correct_answer:
+                    print("Correct!")
+                    if question_number == 1:
+                        money = "$500"
+                    elif question_number == 2:
+                        money = "$1000"
+                    elif question_number == 3:
+                        money = "$2000"
+                    elif question_number == 4:
+                        money = "$3000"
+                    elif question_number == 5:
+                        money = "$5000"
+                    elif question_number == 6:
+                        money = "$7000"
+                    elif question_number == 7:
+                        money = "$10000"
+                    elif question_number == 8:
+                        money = "$20000"
+                    elif question_number == 9:
+                        money = "$30000"
+                    elif question_number == 10:
+                        money = "$5000"
+                    elif question_number == 11:
+                        money = "$100000"
+                    elif question_number == 12:
+                        money = "$250000"
+                    elif question_number == 13:
+                        money = "$500000"
+                    elif question_number == 14:
+                        money = "$1000000"
+                    print("You have " + money + ".\n")
+                else:
+                    print("Game Over.")
+                    continue
+    else:
+        start_screen()
 
 
 def create_questions():

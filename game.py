f527bb7a72e1693f236d73eb9973e39de99625e9
smokedplus <d4nksc0p3@gmail.com>
2023-09-18 17:15:29 -0500

    Modified question maker logic to fix looping system.

--- game.py
+++ game.py
@@ -15,23 +15,23 @@ set_of_questions = []
 
 def create_questions():
     while True:
-        question = input("What question would like to add?\n")
-        correct_answer = input("What is the answer to " + question + "\n")
-        fake_answer1 = input("Input the 1st fake answer.\n")
-        fake_answer2 = input("Input the 2nd fake answer.\n")
-        fake_answer3 = input("Input the 3rd fake answer.\n")
-        temp_question = Question(question, answer, fake_answer1, fake_answer2, fake_answer3)
-        set_of_questions.append(temp_question)
         user_choice2 = input(
-            "Would you like to view the current questions, make another, or go play? Input View, Make, or Play.\n")
+            "Would you like to view the current questions, make a question, or go back to the start screen? Input View, Make, or Back.\n")
         if user_choice2 == "View":
             for question in set_of_questions:
-                print("Q: " + question.question + " A: " + question.answer)
-        elif user_choice2 == "Make":
+                print("Q: " + question.question + " A: " + question.correct_answer+ "\n")
             continue
-        elif user_choice2 == "Play":
-            pass
-
+        elif user_choice2 == "Make":
+            question = input("What question would like to add?\n")
+            correct_answer = input("What is the answer to " + question + "\n")
+            fake_answer1 = input("Input the 1st fake answer.\n")
+            fake_answer2 = input("Input the 2nd fake answer.\n")
+            fake_answer3 = input("Input the 3rd fake answer.\n")
+            temp_question = Question(question, correct_answer, fake_answer1, fake_answer2, fake_answer3)
+            set_of_questions.append(temp_question)
+            continue
+        elif user_choice2 == "Back":
+            break
 
 def start_screen():
     user_choice = input("Do you want to play the game or create a set of questions? Input Play or Create.")
